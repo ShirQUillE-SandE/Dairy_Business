548600e1573685c5da8d3ee21f255ca692463629
shirquille.sande <shirquille.sande@student.moringaschool.com>
2021-05-03 09:18:12 +0300

    debugging JS

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ let totalProduction = function(){
 totalProduction();
 // Stores total number of days for each respective month in a leap year
 let months = {January: 31, February: 29, March: 31, April: 30, May: 31, June: 30, July: 31, August: 31, September: 30, October: 31, November: 30, December: 31}
-// calculate total income on a weekly, daily, monthly and yearly basis
+// Income on a weekly, daily, monthly and yearly basis
 let incomeOverTime = function income(selling_price, time){
     let weeklyTotal = 1;
     let yearlyTotal = 1;
@@ -30,7 +30,7 @@ let incomeOverTime = function income(selling_price, time){
     alert("Your yearly income will be Ksh " + yearlyTotal);
     // Iterates over the months object and prints the total amount of money to be made
     for (const [key, value] of Object.entries(months)) {
-        console.log("Your income for " + key + " is " + value * totalLitres * selling_price);
+        alert("Your income for " + key + " is " + value * totalLitres * selling_price);
       }
 }
 
